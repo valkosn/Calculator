b0c36ae32e5771ba7b541834ce0254e6bcad5bae
valkosn <valkosn1987@gmail.com>
2016-04-20 12:28:30 +0300

    Calc: Implement calculator functionality for small numbers;

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,14 +3,15 @@
  */
 public class StringCalculator implements Calculator {
     public String append(String a, String b) {
-        return null;
+
+        return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
     }
 
     public String subtract(String a, String b) {
-        return null;
+        return Integer.toString(Integer.parseInt(a) - Integer.parseInt(b));
     }
 
     public String multiply(String a, String b) {
-        return null;
+        return Integer.toString(Integer.parseInt(a) * Integer.parseInt(b));
     }
 }
